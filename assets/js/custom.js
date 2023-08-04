4ea29d670f64a4e742097add023556e3f210271e
ifthakhar94 <ifthakhar8@gmail.com>
2023-08-04 12:54:04 +0600

    latest post ui design

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  // Owl Carousel Activation
+  $(".owl-carousel").owlCarousel({
+    items: 2,
+    nav: true,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    margin: 30,
+  });
+});
